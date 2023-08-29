b61f5e177f462a8f6c019d91975e7a03447aa991
RaviTechno <meetravinimbalkar@gmail.com>
2023-08-29 12:41:56 +0530

    Ravi's second spring boot project commit

---
+++ src/main/java/com/ravi/spring_boot_first/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ravi.spring_boot_first.hello;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
